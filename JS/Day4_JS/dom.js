a6121423c97c2af632d377fd0b14bb25399c5595
thhs16 <thaehtapehtarsan@gmail.com>
2024-07-17 20:54:44 +0630

    my_JS_learning_and_practice

--- JS/Day4_JS/dom.js
+++ JS/Day4_JS/dom.js
@@ -1,3 +1,45 @@
    // getElementById
    document.getElementById("demo").innerHTML = "hello world"
    console.log(document); //obj or not
+
+   console.log(document.body);
+   console.log(document.head);
+   console.log(document.URL);
+   console.log(document.location);
+
+   // getElementTagName // collection
+   console.log(document.getElementsByTagName('p'));
+   document.getElementsByTagName('p')[0].innerHTML = 'This is Rola';
+
+   // getElementbyclassName
+   let clsEle = document.getElementsByClassName('greeting');
+   console.log(clsEle);
+   clsEle[0].innerHTML = 'hello this is my program';
+
+   // element.attribute
+   console.log(clsEle[0].attributes);
+   console.log(clsEle[0].attributes[0].nodeValue);
+
+
+   // clsEle[0].attributes[0].nodeValue = 'farewell'; // error
+   // console.log(clsEle[0].attributes[0].nodeValue);
+
+   // editing value in the attribute
+   // clsEle[0].setAttribute ('class', 'farewell'); // error
+   // console.log(clsEle[0].attributes[0].nodeValue);
+
+
+
+   // querySelector
+   console.log(document.querySelector('h1#demo'));
+   document.querySelector('h1#demo').innerHTML = 'hello user';
+
+   // querySelectorAll
+   console.log(document.querySelectorAll('h1')[0]);
+   document.querySelectorAll('h1')[0].innerHTML = 'this is dom test from "querySelectorAll"';
+
+   // changing image
+   document.querySelector('img').src = "https://i.etsystatic.com/9632128/r/il/f40bbd/1169402613/il_570xN.1169402613_lpua.jpg"
+
+   // date()
+   document.write(Date());
---
+++ JS/Day5_JS/changing_color_from_js.js
@@ -0,0 +1,4 @@
+    // let header_text = document.getElementsByTagName('h1');
+    let header_text = document.getElementById('title');
+    console.log(header_text);
+    header_text[0].style.color = 'blue';
