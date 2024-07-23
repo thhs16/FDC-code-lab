a587a9377e58363ab8e635719bde8e07b6220da9
thhs16 <thaehtapehtarsan@gmail.com>
2024-07-23 10:32:01 +0630

    my_JS_learning_and_practice

---
+++ JS/Day6_JS/fetching_jquery.js
@@ -0,0 +1,11 @@
+
+    // ajax 
+    // get : retrieve data from api
+    // post : export data from api
+
+    $(document).ready(function(){
+
+        $('#ajax_btn').click(function(){
+            
+        })
+    })
--- JS/Day6_JS/jQuery.js
+++ JS/Day6_JS/jQuery.js
@@ -1,10 +1,14 @@
 
     // https://cdnjs.com/
+
     console.log('hello from js fle');
+
     $(document).ready(function(){
+
         // jQuery code
-        console.log('this is');
-        $(button).click(function(){
+        console.log('this is'); 
+        $('button').click(function(){ //error
             alert('hello')
+            $('p').hide(); // style.display = 'none'
         })
     })
