a5d2fb4e60adcad579989833a8f55697cf1e6604
thhs16 <thaehtapehtarsan@gmail.com>
2024-08-18 21:24:09 +0630

    PHP_register

--- PHP/Day13_PHP/mini_projects/32_register_PHP.php
+++ PHP/Day13_PHP/mini_projects/32_register_PHP.php
@@ -85,7 +85,6 @@
     <?php 
         require_once('./32_register2_PHP.php')
     ?>
-
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </html>
